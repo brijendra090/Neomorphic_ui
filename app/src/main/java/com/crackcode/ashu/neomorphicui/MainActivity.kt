ba25d4bf8f593182ffc4ce9435fdbdc78aeb8dc2
brijendra <ibmbrijendra93@gmail.com>
2021-08-27 13:14:30 +0530

    Initial commit

---
+++ app/src/main/java/com/crackcode/ashu/neomorphicui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.crackcode.ashu.neomorphicui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
